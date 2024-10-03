9bfefcbd2fa664f2c12e73398c5839c705739130
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2024-10-03 04:38:50 +0000

    Aligned Linkedin image and some cleanup

--- eloys-website/src/App.js
+++ eloys-website/src/App.js
@@ -1,40 +1,42 @@
-import logo from './LinkedinThumbnail.PNG';
+import React from 'react';
+import LinkedinImg from './LinkedinThumbnail.PNG';
 import './App.css';
 
 function App() {
   return (
-    <body>
-    <div class="introduction" >
-        <div class="line">Hello,</div>
-        <div class="line">I am Eloy Lopez</div>
-    </div>
+    <div>
+      <div className="introduction">
+        <p>Hello,<br />
+        I am Eloy Lopez</p>
+      </div>
 
-    <div class="welcome" >
-        <div class="line">Welcome to my website!</div>
-        <div class="line">Here I will showcase all my experience</div>
-        <div class="line">but first...</div>
-    </div>
-
-    <div class="bio" >
-        <div class="line">A little about myself</div>
-        <p>"I graduated from the University of North Texas (GO MEAN GREEN) with a
-            Bachelors in Mechanical and Energy Engineering"
+      <div className="welcome">
+        <p> Welcome to my website!<br /> {/*Enter lines*/}
+        Here I will showcase all my experience
+        but first...
         </p>
-    </div>
+      </div>
 
-    <div class="links" >
-        <div class="line">Now that you know a little about me,</div>
-        <div class="line">what else would you like to see?</div>
-    </div>
+      <div className="bio">
+        <div className="bioHeader">A little about myself</div> 
+        <p> {/*for paragraphs*/}
+          "I graduated from the University of North Texas (GO MEAN GREEN) with a
+          Bachelor's in Mechanical and Energy Engineering"
+        </p>
+      </div>
 
-    <div class="Linkedin" >
-        <a href="https://Linkedin.com/in/lopez-eloy/"
-            target="_blank"> Linkedin
-            <img src={logo} />
+      <div className="links">
+        <p>Now that you know a little about me,
+        what else would you like to see?</p>
+      </div>
+
+      <div className="Linkedin">
+        <a href="https://Linkedin.com/in/lopez-eloy/" target="_blank" rel="noopener noreferrer">
+          Linkedin
+          <img src={LinkedinImg} alt="LinkedIn Thumbnail" />
         </a>
+      </div>
     </div>
-
-</body>
   );
 }
 
