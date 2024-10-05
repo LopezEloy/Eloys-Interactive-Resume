0a2961f8f465f15d343fc769c1bd933658f3dacb
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2024-10-05 05:36:06 +0000

    Added a sidebar with short bio

--- eloys-website/src/App.js
+++ eloys-website/src/App.js
@@ -1,45 +1,30 @@
 import React from 'react';
-import LinkedinImg from './LinkedinThumbnail.PNG';
+import LinkedinIcon from './Linkedin.png';
 import './App.css';
 
 function App() {
   return (
     <div>
       <div className="introduction">
-        Hey there! <br />
+        Hey there! <br />{/*for enter lines*/}
         I'm Eloy Lopez <br />
-        Welcome to my React-folio!
+        Welcome to my React-folio! 
       </div>
 
-      <div className="bio">
-        <div className="bioHeader">A little about myself</div> 
-        <p> {/*for paragraphs*/}
-          "I graduated from the University of North Texas (GO MEAN GREEN) with a
-          Bachelor's in Mechanical and Energy Engineering"
-        </p>
+      <div className="shortBio">
+        
+          Here I will showcase the experience I've gained
+          throughout my time at The University of North Texas
+          up until my current career!
+          Let's travel!
       </div>
 
-      <div className="links">
-        <p>Now that you know a little about me,
-        what else would you like to see?</p>
+      <div className="menuBackground">
       </div>
 
       <div className="Linkedin">
         <a href="https://Linkedin.com/in/lopez-eloy/" target="_blank" rel="noopener noreferrer">
-          Linkedin
-          <img src={LinkedinImg} alt="LinkedIn Thumbnail" />
-        </a>
-      </div>
-
-      <div className="Resume">
-          Eloys Resume
-          <img src={LinkedinImg} alt="LinkedIn Thumbnail" />
-      </div>
-
-      <div className="Github">
-        <a href="https://Linkedin.com/in/lopez-eloy/" target="_blank" rel="noopener noreferrer">
-          Github
-          <img src={LinkedinImg} alt="LinkedIn Thumbnail" />
+          <img src={LinkedinIcon} alt="LinkedIn Thumbnail" />
         </a>
       </div>
 
