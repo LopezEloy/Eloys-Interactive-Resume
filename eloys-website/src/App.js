cabdee844f947b329b5b2b291c836f58bf9d5ae2
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2024-10-14 00:08:20 +0000

    Added first experience section

--- eloys-website/src/App.js
+++ eloys-website/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from './components/Sidebar'; // Import Sidebar component
 import AboutMe from './components/AboutMe'; // Import AboutMe component
 import Introduction from './components/Introduction'; // Import Introduction component
+import Experience from './components/Experience'; // Import Experience component
 import ResumePage from "./components/Resume";
 import './App.css';
 
@@ -16,10 +17,12 @@ function App() {
 
         <AboutMe /> {/* Aboutme component */}
 
+        <Experience /> {/* Experience component */}
+
         <Routes>
           <Route path="/resume" element={<ResumePage />} />
         </Routes>
-        
+
       </div>
     </Router>
   );
---
+++ eloys-website/src/components/Experience.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Experience = () => {
+    return(
+        <div>
+            <div className="experienceHeader"> 
+                Experience
+            </div>
+
+            <div className="experience1-title">
+                Amazon Robotics
+            </div>
+
+            <div className="experience1-role">
+                Contract Deployment Engineer
+            </div>
+
+            <div className="experience1-dates">
+                April 2024 - Present
+            </div>
+
+            <div className="experience1-description">
+                At Amazon Robotics I quality check Amazon technology
+            </div>
+
+        </div>
+    )
+}
+
+export default Experience;
