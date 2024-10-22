2d1e0c6e38fbd45959d2e896cae48beaed2221b4
Eloy <eloylopez67@gmail.com>
2024-10-22 00:46:23 -0500

    Added About section to the json file.

--- eloys-website/src/components/AboutMe.js
+++ eloys-website/src/components/AboutMe.js
@@ -1,33 +1,25 @@
 import React from "react";
+import materialInfo from "../material.json";
+import { Fade } from "react-awesome-reveal";
 import EloyPicture from './media/EloyPic.jpg';
 
 const AboutMe = () => {
+    const { About } = materialInfo;
+
     return(
-        <div>
+        <Fade triggerOnce={true}>
             <div className="aboutmeHeader">
-                About
+                {About.Section.Header}
+                <div className="aboutme">
+                {About.Section.Description}
+                    <div className="aboutme-item" >
+                        <a>
+                        <img src={EloyPicture} />
+                        </a>
+                    </div>
+                </div>
             </div>
-
-            <div className="aboutme">
-                I graduated from the University of North Texas Spring, 2023
-                with a major in Mechanical and Energy Engineering and a
-                minor in Computer Science.<br /><br />
-                I am currently a Contract Deployment Engineer for Amazon Robotics
-                through TEKsystems. There I perform quality checks, troubleshoot, and document
-                Amazon technology to ensure safety and proper functionality for associates.<br /><br />
-                I always enjoyed coding. I've always been fascinated by what one can do with programming.
-                On my free time I am actively acquiring new skills such as React and JavaScript.
-                Creating this portfolio was the first big project I began doing post-graduation and honestly
-                I'm enjoying it.
-
-            </div>
-
-            <div className="aboutme-item" >
-                <a>
-                    <img src={EloyPicture} />
-                </a>
-            </div>
-        </div>
+        </Fade>
     )
 }
 
